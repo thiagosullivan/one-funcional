58f3837a2e0a9a95b9461e47b0e1cc7eb8f0d765
thiagosullivan <thiagosullivan12@gmail.com>
2022-03-24 01:25:59 -0300

    Initializing project and fetching data from datocms

---
+++ pages/blog/[slug].js
@@ -0,0 +1,60 @@
+import ReactMarkdown from "react-markdown";
+import { getAllPosts } from "../../lib/dato-cms";
+
+function PostPage({ post, posts}){
+
+  console.log(post, 'post')
+  console.log(posts, 'posts')
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <img src={post.postImg.url}></img>
+      <ReactMarkdown>{post.postTxt}</ReactMarkdown>
+      {post.videoUrl ?
+        <iframe
+          width="560"
+          height="315"
+          src={post.videoUrl}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+        : ''
+      }
+    </>
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params?.slug;
+  const posts = await getAllPosts();
+  const post = posts.find((s) => s.slug === slug) || null;
+
+  if(!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+      allPosts: posts,
+    },
+    revalidate: 60,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const posts = await getAllPosts();
+  const slugs = posts.map((s) => ({ params: { slug: s.slug }}));
+
+  return {
+    paths: slugs,
+    fallback: false,
+  }
+}
+
+export default PostPage;
