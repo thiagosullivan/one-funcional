2560a9a0394a8b9a40735c32b88bc8b09d6b4f6b
thiagosullivan <thiagosullivan12@gmail.com>
2022-03-24 04:09:21 -0300

    reset css

--- styles/globals.js
+++ styles/globals.js
@@ -1,5 +1,40 @@
+import { lighten } from 'polished';
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    ::-webkit-scrollbar {
+      width: 8px;
+      height: 5px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.primary};
+      border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track{
+      background: ${({ theme }) => lighten(0.4, theme.primary)};
+    }
+  }
+  body {
+    background: ${props => props.theme.background};
+    font: 400 1rem 'Montserrat', sans-serif;
+    overflow-x: hidden;
+  }
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
 `;
