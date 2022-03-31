c998bdfbcf5954ce712cc1b8d5d9b2288288ba46
thiagosullivan <thiagosullivan12@gmail.com>
2022-03-31 18:10:23 -0300

    Pages css

---
+++ styles/blogPage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const BlogPageContainer = styled.section`
+  max-width: 1300px;
+  width: 100%;
+  margin: 6.75rem auto 6.75rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`
---
+++ styles/postPage.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PostPageContainer = styled.main`
+
+`;
+
+export const PostPageContainerBottom = styled.section`
+  max-width: 1300px;
+  width: 100%;
+  margin: 6.75rem auto 0;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
