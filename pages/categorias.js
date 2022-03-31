97ade5cd3f329db856f044aa175d07b595a5b25c
thiagosullivan <thiagosullivan12@gmail.com>
2022-03-31 18:12:05 -0300

    Adding categories page

---
+++ pages/categorias.js
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Head from 'next/head';
+import { getAllCategories, getAllPosts } from "../lib/dato-cms";
+
+import Header from '../components/header';
+import BannerContact from '../components/banners/bannerContact';
+import Footer from '../components/footer';
+import PostCardResults from '../components/postCardResults';
+import Aside from '../components/aside';
+
+import { ResultContainerPage, ResultPage } from '../styles/resultPage';
+
+export async function getStaticProps() {
+  const posts = (await getAllPosts()) || [];
+  const categories = (await getAllCategories() || []);
+
+  return {
+    props: { posts, categories }
+  }
+}
+
+function ResultsPage({ posts, categories }){
+  console.log(posts, 'POSTS result page')
+
+  const router = useRouter()
+  const searchQuery = router.query.s
+  console.log(searchQuery, 'QUERY') 
+
+  const itensFiltrados = posts.filter(({ categories }) => categories.some(({ slug }) => slug.includes(searchQuery)));
+  
+  let filteredPosts = itensFiltrados  
+  console.log(filteredPosts, 'FUNCTION OUT')
+
+  return (
+    <ResultPage id="result__page">
+      <Head>
+        <title>Resultados | One Funcional</title>
+        <meta name="description" content="A Funcional One vem trazer uma novo conceito em treinamento físico para pessoas que querem manter sua boa forma e buscar prevenir-se de novas lesões." />
+        
+      </Head>
+
+      <Header />
+      <BannerContact />
+      <main>
+        <ResultContainerPage>
+          {filteredPosts.length === 0 && (
+            <div className='Search__error'>
+              <p>Desculpe, mas nenhuma publicação foi encontrada nessa busca.</p>
+              <Link href="/">Voltar para a Home</Link>
+            </div>
+          )}
+          <PostCardResults postagens={filteredPosts} />
+        </ResultContainerPage>
+        <Aside categories={categories}/>
+      </main>
+
+      <Footer />
+    </ResultPage>
+  )
+}
+
+export default ResultsPage;
