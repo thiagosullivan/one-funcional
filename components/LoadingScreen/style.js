940adb6a11cb10c12ede86cbbf952b9ee1a20f0f
thiagosullivan <thiagosullivan12@gmail.com>
2022-04-05 00:48:35 -0300

    changes 2

--- components/LoadingScreen/style.js
+++ components/LoadingScreen/style.js
@@ -7,7 +7,7 @@ export const LoadingScreenContainer = styled.section`
   position: absolute;
   width: 100%;
   height: 100%;
-  top: 0;
+  top: 0; 
   left: 0;
   background-color: rgba(0,0,0,.7)
   /* position: fixed; */
